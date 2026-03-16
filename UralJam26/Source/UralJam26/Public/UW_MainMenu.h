5706d6d0700a727c09fd73611a0ad8f7a79e46ac
Chernov Andrey <xWmaximusWx@yandex.ru>
2026-03-16 17:59:10 +0300

    Text 2

--- UralJam26/Source/UralJam26/Public/UW_MainMenu.h
+++ UralJam26/Source/UralJam26/Public/UW_MainMenu.h
@@ -6,29 +6,39 @@
 #include "Blueprint/UserWidget.h"
 #include "UralJam_GameInstance.h"
 #include "Components\Button.h"
+#include "Components\Slider.h"
 #include "UW_MainMenu.generated.h"
 
-class UButton;
+
 class UGame_for_UralJam_GameInstance;
 class AMenu_PlayerController;
-UCLASS()
+UCLASS(Abstract)
 class URALJAM26_API UUW_MainMenu : public UUserWidget
 {
 	GENERATED_BODY()
-	
 protected:
+	virtual void NativeConstruct() override;
+	
+private:
 	UPROPERTY(meta = (BindWidget))
 	UButton* StartGameButton;
 
 	UPROPERTY(meta = (BindWidget))
 	UButton* ExitGameButton;
 
+	UPROPERTY(meta=(BindWidget))
+	USlider* SoundVolumeSlider;
+
 	UFUNCTION()
 	void OnStartGameButtonClicked();
 
 	UFUNCTION()
 	void OnExitGameButtonClicked();
-private:
-	UUralJam_GameInstance* GetUralJam_GameInstance();
-	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void OnMasterVolumeChanged(float Value);
+
+	
+	UPROPERTY()
+	TObjectPtr<UUralJam_GameInstance> UralJam_GameInstance;
 };
