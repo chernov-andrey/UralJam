06a4e75ac21e00855acc6726b05584f2300f7ace
Chernov Andrey <xWmaximusWx@yandex.ru>
2026-03-17 22:14:27 +0300

    Добавил сруктуру сатов персонажа Создал Мастер класс персонажа

--- UralJam26/Source/UralJam26/Private/Game/Master_Character.cpp
+++ UralJam26/Source/UralJam26/Private/Game/Master_Character.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Game\Player_Character.h"
+#include "Game\Master_Character.h"
 
 // Sets default values
-APlayer_Character::APlayer_Character()
+AMaster_Character::AMaster_Character()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,21 +12,21 @@ APlayer_Character::APlayer_Character()
 }
 
 // Called when the game starts or when spawned
-void APlayer_Character::BeginPlay()
+void AMaster_Character::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void APlayer_Character::Tick(float DeltaTime)
+void AMaster_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
 // Called to bind functionality to input
-void APlayer_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AMaster_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
--- UralJam26/Source/UralJam26/Public/Game/Master_Character.h
+++ UralJam26/Source/UralJam26/Public/Game/Master_Character.h
@@ -4,18 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Player_Character.generated.h"
+#include "Structures/CharacterStats.h"
+#include "Intrfaces/Damageable_Interface.h"
+#include "Master_Character.generated.h"
 
-UCLASS()
-class URALJAM26_API APlayer_Character : public ACharacter
+
+UCLASS(Abstract)
+class URALJAM26_API AMaster_Character : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this character's properties
-	APlayer_Character();
+	AMaster_Character();
+	
 
-protected:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Character Stats")
+	FCharacterStats Stats;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
@@ -25,5 +31,4 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
